cd249713ab0904093a65c88828a53ee7bb672bd0
BrianH <brianhwang29@gmail.com>
2021-06-24 15:00:25 +0900

    search, repo add findByTitleContaining

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ## CRUD bulletin board
+###### springBoot + JPA + MySQL + Thymeleaf
 
 - [x] project setup
 - [x] write bulletin 
 - [x] lookup bulletin
 - [x] edit & delete
-- [ ] search
+- [x] search
 - [ ] paging 
+
+- [reference][https://victorydntmd.tistory.com/333]
--- src/main/java/com/bh/bulletin/repository/BoardRepository.java
+++ src/main/java/com/bh/bulletin/repository/BoardRepository.java
@@ -3,5 +3,9 @@ package com.bh.bulletin.repository;
 import com.bh.bulletin.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    //JpaRepository에서는 By 뒷 부분은 SQL의 where 조건 절에 해당된다. 따라서, Containing을 붙여주면 Like 검색이 된다.
+    List<Board> findByTitleContaining(String keyword);
 }
